0a49da02a9bf985cdc837eea3b8a2babf7e034d9
MartinStoev00 <martinstoev00@gmail.com>
2021-05-31 17:44:01 +0200

    init

---
+++ BasicParsers.hs
@@ -0,0 +1,81 @@
+-- Add your names and student numbers to the following file. Do not change anything else, since it is parsed.
+-- Student 1: Martin Stoev (s2392593)
+-- Student 2: Remy Benitah (s2247372)
+
+module BasicParsers where
+import Control.Applicative
+import Data.Char
+import Data.Monoid
+import Test.QuickCheck
+import PComb
+
+-- FP2.1
+letters = ['a'..'z'] ++ ['A'..'Z']
+digits = ['0'..'9']
+-- parses any character
+letter :: Parser Char
+letter = P p 
+    where p (Stream [])                        = []
+          p (Stream (x:xs)) | x `elem` letters = [(x, Stream xs)]
+                            | otherwise        = []
+-- parses any digit
+dig :: Parser Char
+dig = P p 
+    where p (Stream [])                       = []
+          p (Stream (x:xs)) | x `elem` digits = [(x, Stream xs)]
+                            | otherwise       = []
+
+-- FP2.2
+between :: Parser a -> Parser b -> Parser c -> Parser b
+between a b c = (\x y z -> y) <$> a <*> b <*> c
+
+whiteParser :: Parser Char
+whiteParser = char '\t' <|> char '\n' <|> char ' '
+
+whitespace :: Parser a -> Parser a
+whitespace parser = (many whiteParser *> parser) <* many whiteParser
+
+-- FP2.3
+sep1 :: Parser a -> Parser b -> Parser [a]
+sep1 p s = some (p <* s <|> p)
+
+sep :: Parser a -> Parser b -> Parser [a]
+sep p s = many (p <* s <|> p)
+
+
+-- TODO: Ask a TA to check it
+option :: a -> Parser a -> Parser a
+option x p = p <|> pure x
+
+-- FP2.4
+---------------------------------
+string :: String -> Parser String
+string str = P p 
+    where p (Stream [])                     = []
+          p (Stream xs)     | inList str xs = [(str, (Stream (drop (length str) xs)))]
+                            | otherwise     = []
+
+    -- Helper function for string, checks if a string is at the front of a list of characters.
+inList :: String -> [Char] -> Bool
+inList [] _ = True
+inList (s:ss) (x:xs)
+                      | s == x      = inList ss xs
+                      | otherwise   = False
+---------------------------------
+identifier :: Parser String 
+identifier = whitespace ((\x y -> [x]++y) <$> letter <*> (many (letter <|> dig)))
+-- identifier = whitespace (some (letter <|> dig))
+---------------------------------
+integer :: Parser Integer
+integer = (\x -> read x :: Integer) <$> whitespace (some (dig))
+
+symbol :: String -> Parser String
+symbol str = whitespace (string str)
+---------------------------------
+parens :: Parser a -> Parser a 
+parens p = (char '(' *> p) <* char ')'
+
+braces :: Parser a -> Parser a 
+braces p = (char '[' *> p) <* char ']'
+
+-- TODO Ask a TA about efficiency 
---
+++ PComb.hs
@@ -0,0 +1,57 @@
+-- Add your names and student numbers to the following file. Do not change anything else, since it is parsed.
+-- Student 1: Martin Stoev (s2392593)
+-- Student 2: Remy Benitah (s2247372)
+
+module PComb where
+import Control.Applicative
+import Data.Char
+import Data.Monoid
+import Test.QuickCheck
+
+-- FP1.1 
+data Stream = Stream [Char]
+              deriving (Eq, Show)
+
+data Parser r = P { 
+    runParser :: Stream -> [(r, Stream)]
+}
+
+-- FP1.2
+instance Functor Parser where
+  fmap f p = P (\x -> [(f r, s) | (r, s) <- runParser p x])       
+
+
+-- FP1.3
+char :: Char -> Parser Char
+char c = P p 
+    where
+        p (Stream [])                   = []
+        p (Stream (x:xs))   | c == x    = [(x, (Stream xs))]
+                            | otherwise = []
+
+-- Testing:
+testChar = runParser (char 's') (Stream "saa")
+
+-- FP1.4
+failure :: Parser a 
+failure = P (\_ -> [])
+
+-- FP1.5
+instance Applicative Parser where
+    pure x  = P (\stream -> [(x, stream)])   -- pure
+    x <*> y = P (\stream -> [(r p, s') | (r, s) <- runParser x stream, (p, s') <- runParser y s]) -- composition
+
+-- Testing:
+-- runParser testApp (Stream "saa")
+testApp = (\x y -> [x,y]) <$> (char 's') <*> (char 'a') 
+
+-- testApp = (\x y z -> y) <$> (char 's') <*> (char 'a') <*> (char 'a')
+
+-- FP1.6
+instance Alternative Parser where
+    empty = P (\_ -> [])
+    p1 <|> p2 = P (\stream -> if (null (runParser p1 stream)) 
+        then (runParser p2 stream) 
+        else (runParser p1 stream))
+
+-- runParser (char 'a' <|> char 'b') (Stream "ba")
